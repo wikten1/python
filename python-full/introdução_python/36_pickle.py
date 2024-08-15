b384591d049247b51428cf2b9856e3a97ad94143
Wikten Alves <wiktenalves@gmail.com>
2024-08-15 11:05:06 -0300

    pickle

--- python-full/introdução_python/36_pickle.py
+++ python-full/introdução_python/36_pickle.py
@@ -1,6 +1,39 @@
+# import pickle
+
+# x = [1,2,3,4]
+
+# string = pickle.dumps(x)
+# print(pickle.loads(string))
+#-----------------------------------
+# import pickle
+# x = [1,2,3,4]
+
+# arq = open('arquivo.pkl', 'wb')
+# pickle.dump(x, arq)
+
+# arq = open('arquivo.pkl', 'rb')
+# retornou = pickle.load(arq)
+
+# print(retornou)
+
+#------------------------------------
+
 import pickle
+class Pessoa:
+    nome = 'caio'
+    idade = 20
 
-x = [1,2,3,4]
+class Pessoas:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+p1 = Pessoas('marcos', 21)
 
-string = pickle.dumps(x)
-print(pickle.loads(string))
+arq = open('arquivo.pkl', 'wb')
+pickle.dump(p1, arq)
+
+arq = open('arquivo.pkl', 'rb')
+retornou = pickle.load(arq)
+
+print(retornou)
