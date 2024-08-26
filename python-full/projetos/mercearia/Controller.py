0c19f47ca4440ee2a56e4a816941b72534a6884b
Wikten Alves <wiktenalves@gmail.com>
2024-08-26 00:59:55 -0300

    Classe controllerCategoria criada

---
+++ python-full/projetos/mercearia/Controller.py
@@ -0,0 +1,17 @@
+from Models import Categoria, Estoque, Produtos, Fornecedor, Pessoa, Funcionario, Venda
+from DAO import DaoCategoria, DaoVenda, DaoEstoque, DaoFornecedor, DaoPessoa, DaoFuncionario
+from datetime import datetime
+
+class ControllerCategoria:
+    def cadastraCategoria(self, novaCategoria):
+        existe = False
+        x = DaoCategoria.ler()
+        for i in x:
+            if i.categoria == novaCategoria:
+                existe = True
+        
+        if not existe:
+            DaoCategoria.salvar(novaCategoria)
+            print('Categoria cadastrada com sucesso!')
+        else:
+            print('A categoria que deseja cadastrar já existe')
