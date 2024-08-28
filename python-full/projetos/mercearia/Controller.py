9f036828d3d16ee71dae338104a5d9922ebb4364
Wikten Alves <wiktenalves@gmail.com>
2024-08-28 11:25:52 -0300

    Função alterar categoria criada

--- python-full/projetos/mercearia/Controller.py
+++ python-full/projetos/mercearia/Controller.py
@@ -33,3 +33,24 @@ class ControllerCategoria:
                 for i in x:
                     arq.writelines(i.categoria)
                     arq.writelines('\n')
+        
+    def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
+        x = DaoCategoria.ler()
+        
+        cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
+        
+        if len(cat > 0):
+            cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
+            if len(cat1) == 0:
+                x = list(map(lambda x: Categoria(categoriaAlterada) if(x.categoria == categoriaAlterar) else(x) ,x))
+                print("A alteração foi efetuada com sucesso!")
+            else:
+                print('A categoria para qual deseja alterar já existe!')
+            
+        else:
+            print('A categoria que deseja alterar não existe!')
+            
+        with open('catagoria.txt', 'w') as arq:
+            for i in x:
+                arq.writelines(i.categoria)
+                arq.writelines('\n')
