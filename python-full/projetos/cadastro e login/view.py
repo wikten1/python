65dc133a9ea8457c80b202aab6f0d16fe0c4bb83
Wikten Alves <wiktenalves@gmail.com>
2025-02-28 04:02:45 -0300

    Projeto prático 2  -  sistema de login
    Projeto prático 2  -  sistema de login finalizado

---
+++ python-full/projetos/cadastro e login/view.py
@@ -0,0 +1,33 @@
+from controller import ControllerCadastro, ControllerLogin
+while True:
+    print("========== [MENU] ==========")
+    decidir = int(input('Digite 1 para cadastrar\nDigite 2 para Logar\nDigite 3 para sair'))
+
+    if decidir == 1:
+        nome = input('Digite seu nome: ')
+        email = input('Digite seu email: ')
+        senha = input('Digite sua senha: ')
+        resultado = ControllerCadastro.cadastrar(nome, email, senha)
+
+        if resultado == 2:
+            print("Tamanho do nome digitado inválido")
+        elif resultado == 3:
+            print("Email maior que 200 caracteres")
+        elif resultado == 4:
+            print("Tamanho da senha inválida")
+        elif resultado == 5:
+            print("Email já cadastrado")
+        elif resultado == 6:
+            print("Erro interno do sistema")
+        elif resultado == 1:
+            print("Cadastro realizado com sucesso")
+    elif decidir == 2:
+        email = input('Digite seu email: ')
+        senha = input('Digite sua senha: ')
+        resultado = ControllerLogin.login(email, senha)
+        if not resultado:
+            print('Email ou senha inválidos')
+        else:
+            print(resultado)
+    else:
+        break
