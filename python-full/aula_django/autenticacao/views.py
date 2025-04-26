40c7171ed60f6ecac360db4b9f7f3ae3b1dbdd84
Wikten Alves <wiktenalves@gmail.com>
2025-04-26 16:50:38 -0300

    Acessando estruturas de dados em um template

--- python-full/aula_django/autenticacao/views.py
+++ python-full/aula_django/autenticacao/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    nome = 'Wikten Alves'
-    idade = '26'
-    profissao = 'Programador'
-    dicionario = {'nome': nome, 'idade': idade, 'profissao': profissao}
-    return render(request, 'cadastro/index.html', dicionario)
+    pessoa = [{'nome': 'Wikten Alves', 'idade': '26', 'profissao': 'Programador'}, {'nome': 'Marcos sampaio', 'idade': '21', 'profissao': 'Escritor'}]
+    
+    return render(request, 'cadastro/index.html', {'pessoa': pessoa})
 
