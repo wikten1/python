f350fa61ead84664a6368a0b397acb31c9cbceb6
Wikten Alves <wiktenalves@gmail.com>
2025-04-26 16:39:27 -0300

    Enviando dados para o template

--- python-full/aula_django/autenticacao/views.py
+++ python-full/aula_django/autenticacao/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'cadastro/index.html')
+    nome = 'Wikten Alves'
+    idade = '26'
+    profissao = 'Programador'
+    dicionario = {'nome': nome, 'idade': idade, 'profissao': profissao}
+    return render(request, 'cadastro/index.html', dicionario)
 
