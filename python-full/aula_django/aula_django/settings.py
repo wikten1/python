009ad6a99a71ec406e5745a57eecbac3af776cbf
Wikten Alves <wiktenalves@gmail.com>
2025-04-26 21:51:47 -0300

    Bug resolvido

--- python-full/aula_django/aula_django/settings.py
+++ python-full/aula_django/aula_django/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]  # correto
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # só usado em produção
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
