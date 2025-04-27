9127d5483e69346fdf1a8cb5dc7f01c0f4b06d53
Wikten Alves <wiktenalves@gmail.com>
2025-04-26 21:19:10 -0300

    arquivos estaticos introdução

--- python-full/aula_django/aula_django/settings.py
+++ python-full/aula_django/aula_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-d+^&#$spx@x1)m8mf^r^ecuya+3!+r*qdah2u4ut)wuoifrubq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'aula_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,9 +115,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
 
-STATIC_URL = 'static/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- python-full/aula_django/autenticacao/views.py
+++ python-full/aula_django/autenticacao/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from aula_django.settings import BASE_DIR
+import os
 
 def cadastro(request):
+    print(BASE_DIR)
     pessoa = [{'nome': 'Wikten Alves', 'idade': '26', 'profissao': 'Programador'}, {'nome': 'Marcos sampaio', 'idade': '21', 'profissao': 'Escritor'}]
     
-    return render(request, 'cadastro/index.html', {'pessoa': pessoa})
+    return render(request, 'cadastro/index.html', {'pessoas': pessoa, 'x': 0})
 
