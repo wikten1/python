2ca4c4dc3d0bb08b448675e06e065a080ff25fa2
Wikten Alves <wiktenalves@gmail.com>
2025-04-29 13:48:48 -0300

    Criando tabelas no banco de dados

--- python-full/aula_django/autenticacao/urls.py
+++ python-full/aula_django/autenticacao/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro', views.cadastro),
-    path('valida_formulario', views.valida_formulario, name="valida_formulario")
+    path('cadastro', views.cadastro, name="cadastro"),
+    
 ]
--- python-full/aula_django/autenticacao/views.py
+++ python-full/aula_django/autenticacao/views.py
@@ -4,11 +4,9 @@ import json
 
 
 def cadastro(request):
-    erro = request.GET.get('erro')
-    
-    return render(request, 'cadastro/index.html', {'erro': erro})
-
-def valida_formulario(request):
-    nome = request.POST.get('nome')
-    email = request.POST.get('email')
-    return HttpResponse(json.dumps({'nome': nome, 'email': email}))
+    if request.method == "GET":
+        return render(request, 'cadastro/index.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        return HttpResponse(json.dumps({'nome': nome, 'email': email}))
