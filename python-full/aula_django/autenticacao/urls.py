2902350b2f46fb385e5023c9063bf967820ff58b
Wikten Alves <wiktenalves@gmail.com>
2025-04-30 04:08:06 -0300

    Banco de dados django

--- python-full/aula_django/autenticacao/urls.py
+++ python-full/aula_django/autenticacao/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro', views.cadastro, name="cadastro"),
-    
+    path('cadastro', views.cadastro, name= "cadastro"),
+    path('listar', views.listar, name = 'listar')
 ]
--- python-full/aula_django/autenticacao/views.py
+++ python-full/aula_django/autenticacao/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
-
+from .models import Pessoa
 
 def cadastro(request):
     if request.method == "GET":
@@ -9,4 +9,16 @@ def cadastro(request):
     elif request.method == "POST":
         nome = request.POST.get('nome')
         email = request.POST.get('email')
-        return HttpResponse(json.dumps({'nome': nome, 'email': email}))
+        senha = request.POST.get('senha')
+    
+        pessoa = Pessoa(nome = nome, email = email, senha = senha)
+        
+        pessoa.save()
+        return HttpResponse('Você foi cadastrado!')
+    
+def listar(request):
+    pessoa = Pessoa.objects.filter(nome = 'marcos')[0]
+    pessoa.delete()
+    
+    pessoas = Pessoa.objects.all()
+    return render(request, 'listar/listar.html', {'pessoa': pessoas})
